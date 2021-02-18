89b38743f62df4cf0b6ab8200e3f9f05fdbae39f
parththakre <48657954+parththakre@users.noreply.github.com>
2021-02-19 02:44:09 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Card from './Card';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  	<App />
   </React.StrictMode>,
   document.getElementById('root')
 );
